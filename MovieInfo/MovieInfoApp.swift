f3dc0da50726c01b2128d506d43ef1b98dd8624e
OfficialJamalJones <jamalamanjones@gmail.com>
2023-01-05 12:33:10 -0500

    Initial Commit

---
+++ MovieInfo/MovieInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieInfoApp.swift
+//  MovieInfo
+//
+//  Created by Consultant on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
